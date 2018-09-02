2898a173e40d1cae1dd146f57d906509fc490931
fewdji <fewdji@gmail.com>
2018-09-02 14:54:35 +0300

    generated config file

---
+++ src/config/ii-cache.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    'driver' => 'gd',
+
+    'images_path' => 'storage/images',
+
+    'presets' => [
+        'hq' => [
+            'widen' => '720',
+            'watermark' => 'default'
+        ],
+
+        'sd' => [
+            'resize' => ['120', '120'],
+            'quality' => '85'
+        ]
+    ],
+
+    'watermarks' => [
+        'default' => [
+            'path' => 'img/copy.png',
+            'size' => '10',
+            'position' => 'bottom-right',
+            'opacity' => '90'
+        ]
+    ]
+
+];
